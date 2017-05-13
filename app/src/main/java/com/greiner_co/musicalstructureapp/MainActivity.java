b6a864129bc77892a293f943cf2bd17a99fd5863
Jens Greiner <jens.greiner@me.com>
2017-05-13 20:16:42 +0200

    added drawable image resources, completed main activity

--- app/src/main/java/com/greiner_co/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/greiner_co/musicalstructureapp/MainActivity.java
@@ -1,7 +1,8 @@
 package com.greiner_co.musicalstructureapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView description = (TextView) findViewById(R.id.main_text_view);
+        String descriptionText = getResources().getString(R.string.main_description);
+        description.setText(descriptionText);
     }
 }
